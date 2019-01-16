bf83ccd663b0a8eda52bc9e8840f03ca3aa51c29
Evgeni Pandurski <epandurski@gmail.com>
2019-01-16 15:07:22 +0200

    Move Configuration to __init__.py

--- swaptacular_debtor/__init__.py
+++ swaptacular_debtor/__init__.py
@@ -4,13 +4,24 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 from flask import Flask  # noqa: E402
+from flask_env import MetaFlaskEnv  # noqa: E402
 from . import extensions  # noqa: E402
 from . import models  # TODO: This is not the place to import modules!
 
 
-def create_app(config_dict={}):
-    from .config import Configuration
+class Configuration(metaclass=MetaFlaskEnv):
+    PORT = 8000
+    SECRET_KEY = 'dummy-secret'
+    SQLALCHEMY_DATABASE_URI = ''
+    SQLALCHEMY_POOL_SIZE = None
+    SQLALCHEMY_POOL_TIMEOUT = None
+    SQLALCHEMY_POOL_RECYCLE = None
+    SQLALCHEMY_MAX_OVERFLOW = None
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
 
+
+def create_app(config_dict={}):
     app = Flask(__name__)
     app.config.from_object(Configuration)
     app.config.from_mapping(config_dict)
