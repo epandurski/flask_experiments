cb56e2accc244ca54ae66b302d8644ef0a595002
Evgeni Pandurski <epandurski@gmail.com>
2019-01-19 16:13:40 +0200

    Work on db schema

--- swaptacular_debtor/models.py
+++ swaptacular_debtor/models.py
@@ -37,8 +37,18 @@ class Debtor(db.Model):
 class Account(db.Model):
     debtor_id = db.Column(db.BigInteger, db.ForeignKey('debtor.debtor_id'), primary_key=True)
     creditor_id = db.Column(db.BigInteger, primary_key=True)
-    amount = db.Column(db.BigInteger, nullable=False)
-
+    balance = db.Column(
+        db.BigInteger,
+        nullable=False,
+        default=0,
+        comment="The total owed amount",
+    )
+    avl_balance = db.Column(
+        db.BigInteger,
+        nullable=False,
+        default=0,
+        comment="The total owed amount minus all pending transaction locks"
+    )
     debtor = db.relationship('Debtor')
 
 
@@ -46,46 +56,19 @@ class PendingTransaction(db.Model):
     debtor_id = db.Column(db.BigInteger, primary_key=True)
     creditor_id = db.Column(db.BigInteger, primary_key=True)
     pending_transaction_seqnum = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
-
+    locked_amount = db.Column(db.BigInteger, nullable=False, default=0)
     __table_args__ = (
         db.ForeignKeyConstraint(
             ['debtor_id', 'creditor_id'],
-            ['account.debtor_id', 'account.creditor_id']
-        ),
-    )
-
-    account = db.relationship('Account')
-    # account_hold = db.relationship('AccountHold', cascade="all, delete-orphan", passive_deletes=True)
-
-
-class AccountHold(db.Model):
-    debtor_id = db.Column(db.BigInteger, primary_key=True)
-    creditor_id = db.Column(db.BigInteger, primary_key=True)
-    pending_transaction_seqnum = db.Column(db.BigInteger, primary_key=True)
-    amount = db.Column(db.BigInteger, nullable=False)
-
-    __table_args__ = (
-        db.ForeignKeyConstraint(
-            [
-                'debtor_id',
-                'creditor_id',
-                'pending_transaction_seqnum',
-            ],
-            [
-                'pending_transaction.debtor_id',
-                'pending_transaction.creditor_id',
-                'pending_transaction.pending_transaction_seqnum',
-            ],
+            ['account.debtor_id', 'account.creditor_id'],
             ondelete='CASCADE',
         ),
     )
 
-    # account = db.relationship(
-    #     'Account',
-    #     foreign_keys=[debtor_id, creditor_id],
-    #     primaryjoin=(debtor_id == Account.debtor_id) & (creditor_id == Account.creditor_id),
-    # )
-    pending_transaction = db.relationship('PendingTransaction')
+    account = db.relationship(
+        'Account',
+        backref=db.backref('pending_transactions', cascade="all, delete-orphan", passive_deletes=True),
+    )
 
 
 class Transaction(db.Model):
@@ -93,12 +76,15 @@ class Transaction(db.Model):
     creditor_id = db.Column(db.BigInteger, primary_key=True)
     transaction_seqnum = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
     amount = db.Column(db.BigInteger, nullable=False)
-
     __table_args__ = (
         db.ForeignKeyConstraint(
             ['debtor_id', 'creditor_id'],
-            ['account.debtor_id', 'account.creditor_id']
+            ['account.debtor_id', 'account.creditor_id'],
+            ondelete='CASCADE',
         ),
     )
 
-    account = db.relationship('Account')
+    account = db.relationship(
+        'Account',
+        backref=db.backref('transactions', cascade="all", passive_deletes=True),
+    )
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,6 @@
 import pytest
-from swaptacular_debtor.models import generate_random_sharding_key, make_sharding_key, ShardingKey
+from swaptacular_debtor.models import generate_random_sharding_key, make_sharding_key, ShardingKey,\
+    Debtor, Account, PendingTransaction
 
 
 def generate_get_random_sharding_key():
@@ -22,3 +23,12 @@ def test_make_sharding_key(db_session):
 @pytest.mark.models
 def test_no_sharding_keys(db_session):
     assert len(ShardingKey.query.all()) == 0
+
+
+def test_acclunt_hold(db_session):
+    d = Debtor(debtor_id=make_sharding_key())
+    a = Account(debtor=d, creditor_id=666, balance=10)
+    pt = PendingTransaction(account=a)
+    db_session.add(pt)
+    db_session.commit()
+    assert PendingTransaction.query.get((d.debtor_id, 666, pt.pending_transaction_seqnum)).locked_amount == 0
