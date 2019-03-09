f95cfecfe056871f1a640147944075106fb94d7b
Evgeni Pandurski <epandurski@gmail.com>
2019-03-09 10:16:26 +0200

    delegate broker (work in progress)

--- swaptacular_debtor/flask_dramatiq.py
+++ swaptacular_debtor/flask_dramatiq.py
@@ -1,36 +1,163 @@
 import threading
 import dramatiq
+import importlib
+import functools
 from dramatiq.brokers import stub
 
 
-def _delegate_actor_method(method_name):
-    def f(instance, *args, **kwargs):
-        actor = instance._actor
-        if actor:
-            return getattr(actor, method_name)(*args, **kwargs)
-        raise RuntimeError('init_app() must be called on brokers before usage')
-    return f
+def _raise_error(e, *args, **kwargs):
+    raise e
 
 
-class LazyActor:
-    def __init__(self, *args, **kwargs):
-        kwargs.pop('broker')
-        self._args = args
-        self._kwargs = kwargs
-        self._actor = None
+def _create_broker(module_name, class_name, default_url):
+    try:
+        module = importlib.import_module(module_name)
+    except ImportError as e:
+        return type(class_name, (), dict(
+            __init__=functools.partial(_raise_error, e),
+        ))
+    class_ = getattr(module, class_name)
+    return type(class_name, (_Dramatiq, class_), dict(
+        _broker_default_url=default_url,
+        _broker_factory=class_,
+    ))
+
+
+class _LazyActor(dramatiq.Actor):
+    def __init__(self, fn, **kw):
+        kw.pop('broker')
+        self.__fn = fn
+        self.__kw = kw
+        self.__actor = None
 
     def register(self, broker):
-        self._actor = dramatiq.Actor(*self._args, broker=broker, **self._kwargs)
+        self.__actor = dramatiq.Actor(self.__fn, broker=broker, **self.__kw)
 
-    for m in [
-        'message',
-        'message_with_options',
-        'send',
-        'send_with_options',
-        '__call__',
-    ]:
-        vars()[m] = _delegate_actor_method(m)
-    del m
+    def __call__(self, *args, **kwargs):
+        if self.__actor:
+            return self.__actor(*args, **kwargs)
+        return self.__fn(*args, **kwargs)
+
+    def __repr__(self):
+        if self.__actor:
+            return repr(self.__actor)
+        return object.__repr__(self)
+
+    def __str__(self):
+        if self.__actor:
+            return str(self.__actor)
+        return object.__str__(self)
+
+    def __get_actor(self):
+        if self.__actor:
+            return self.__actor
+        raise RuntimeError('The init_app() method must be called on brokers before use.')
+
+    # Delegate attribute access to the underlying actor instance.
+    def __getattr__(self, name):
+        return getattr(self.__get_actor(), name)
+
+    def __setattr__(self, name, value):
+        if name.startswith('_LazyActor__'):
+            return object.__setattr__(self, name, value)
+        return setattr(self.__get_actor(), name, value)
+
+    def __delattr__(self, name):
+        if name.startswith('_LazyActor__'):
+            return object.__delattr__(self, name)
+        return delattr(self.__get_actor(), name)
+
+
+class _Dramatiq:
+    __registered_config_prefixes = set()
+
+    def __init__(self, app=None, config_prefix='DRAMATIQ_BROKER', **options):
+        if config_prefix in self.__registered_config_prefixes:
+            raise RuntimeError(
+                'Can not create a second broker with config prefix "{}". '
+                'Did you forget to pass the "config_prefix" argument when '
+                'creating the broker?'.format(config_prefix)
+            )
+        self.__registered_config_prefixes.add(config_prefix)
+        self.__config_prefix = config_prefix
+        self.__options = options
+        self.__unregistered_lazy_actors = []
+        self.__broker_url = None
+        self.__broker = None
+        self.__stub = stub.StubBroker(middleware=options.get('middleware'))
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        broker_url = self.__read_url_from_config(app)
+        if self.__stub:
+            self.__stub.close()
+            self.__stub = None
+            broker = self._broker_factory(url=broker_url, **self.__options)
+            broker.add_middleware(AppContextMiddleware(app))  # TODO: detect multiple apps?
+            for actor in self.__unregistered_lazy_actors:
+                actor.register(broker=broker)
+            self.__unregistered_lazy_actors.clear()
+            self.__broker_url = broker_url
+            self.__broker = broker
+            if self.__config_prefix == 'DRAMATIQ_BROKER':
+                dramatiq.set_broker(self)
+        if broker_url != self.__broker_url:
+            raise RuntimeError(
+                '{app} tried to start a broker with '
+                '{config_prefix}_URL={new_url}, '
+                'but another app already has started that broker with '
+                '{config_prefix}_URL={old_url}.'.format(
+                    app=app,
+                    config_prefix=self.__config_prefix,
+                    new_url=broker_url,
+                    old_url=self.__broker_url,
+                )
+            )
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions[self.__config_prefix.lower()] = self
+
+    def actor(self, fn=None, **kwargs):
+        for kw in ['broker', 'actor_class']:
+            if kw in kwargs:
+                raise TypeError("actor() got an unexpected keyword argument '{}'".format(kw))
+
+        def decorator(fn):
+            if self.__broker:
+                return dramatiq.actor(broker=self.__broker, **kwargs)(fn)
+            lazy_actor = dramatiq.actor(actor_class=_LazyActor, broker=self.__stub, **kwargs)(fn)
+            self.__unregistered_lazy_actors.append(lazy_actor)
+            return lazy_actor
+
+        if fn is None:
+            return decorator
+        return decorator(fn)
+
+    def __read_url_from_config(self, app):
+        return (
+            app.config.get('{0}_URL'.format(self.__config_prefix))
+            or self._broker_default_url
+        )
+
+    def __get_broker(self):
+        if self.__broker:
+            return self.__broker
+        raise RuntimeError('The init_app() method must be called on brokers before use.')
+
+    # Delegate attribute access to the underlying broker instance.
+    def __getattr__(self, name):
+        return getattr(self.__get_broker(), name)
+
+    def __setattr__(self, name, value):
+        if name.startswith('_Dramatiq__'):
+            return object.__setattr__(self, name, value)
+        return setattr(self.__get_broker(), name, value)
+
+    def __delattr__(self, name):
+        if name.startswith('_Dramatiq__'):
+            return object.__delattr__(self, name)
+        return delattr(self.__get_broker(), name)
 
 
 class AppContextMiddleware(dramatiq.Middleware):
@@ -55,102 +182,15 @@ class AppContextMiddleware(dramatiq.Middleware):
     after_skip_message = after_process_message
 
 
-class _Dramatiq:
-    _registered_config_prefixes = set()
-
-    def __init__(self, app=None, config_prefix='DRAMATIQ_BROKER', **options):
-        if config_prefix in self._registered_config_prefixes:
-            raise RuntimeError(
-                'Can not create a second broker with config prefix "{}". '
-                'Did you forget to pass the "config_prefix" argument when '
-                'creating the broker?'.format(config_prefix)
-            )
-        self._registered_config_prefixes.add(config_prefix)
-        self.config_prefix = config_prefix
-        self.options = options
-        self.actors = []
-        self.broker_url = None
-        self.broker = None
-        self._stub = stub.StubBroker(middleware=options.get('middleware'))
-        if app is not None:
-            self.init_app(app)
-
-    def init_app(self, app):
-        broker_url = self.get_broker_url(app)
-        if self._stub:
-            self._stub.close()
-            self._stub = None
-            broker = self._broker_factory(url=broker_url, **self.options)
-            broker.add_middleware(AppContextMiddleware(app))
-            for actor in self.actors:
-                actor.register(broker=broker)
-            dramatiq.set_broker(broker)  # TODO: this is probably not OK!
-            self.broker_url = broker_url
-            self.broker = broker
-        if broker_url != self.broker_url:
-            raise RuntimeError(
-                '{app} tried to start a broker with '
-                '{config_prefix}_URL={new_url}, '
-                'while another app has already started that broker with '
-                '{config_prefix}_URL={old_url}.'.format(
-                    app=app,
-                    config_prefix=self.config_prefix,
-                    new_url=broker_url,
-                    old_url=self.broker_url,
-                )
-            )
-        if not hasattr(app, 'extensions'):
-            app.extensions = {}
-        app.extensions[self.config_prefix.lower()] = self.broker
-
-    def get_broker_url(self, app):
-        return (
-            app.config.get('{0}_URL'.format(self.config_prefix))
-            or self._broker_default_url
-        )
-
-    def actor(self, fn=None, **kwargs):
-        for kw in ['broker', 'actor_class']:
-            if kw in kwargs:
-                raise TypeError("actor() got an unexpected keyword argument '{}'".format(kw))
-
-        def decorator(fn):
-            if self.broker:
-                broker = self.broker
-                actor_class = dramatiq.Actor
-            else:
-                broker = self._stub
-                actor_class = LazyActor
-            actor = dramatiq.actor(actor_class=actor_class, broker=broker, **kwargs)(fn)
-            self.actors.append(actor)
-            return actor
-
-        if fn is None:
-            return decorator
-        return decorator(fn)
+RabbitmqBroker = _create_broker(
+    module_name='dramatiq.brokers.rabbitmq',
+    class_name='RabbitmqBroker',
+    default_url='amqp://127.0.0.1:5672',
+)
 
 
-class StubBroker(_Dramatiq, stub.StubBroker):
-    _broker_default_url = 'stub://'
-
-    @staticmethod
-    def _broker_factory(middleware=None, *args, **kwargs):
-        return stub.StubBroker(middleware=middleware)
-
-
-class RabbitmqBroker(_Dramatiq):
-    _broker_default_url = 'amqp://127.0.0.1:5672'
-
-    @property
-    def _broker_factory(self):
-        import dramatiq.brokers.rabbitmq
-        return dramatiq.brokers.rabbitmq.RabbitmqBroker
-
-
-class RedisBroker(_Dramatiq):
-    _broker_default_url = 'redis://127.0.0.1:6379/0'
-
-    @property
-    def _broker_factory(self):
-        import dramatiq.brokers.redis
-        return dramatiq.brokers.redis.RedisBroker
+RedisBroker = _create_broker(
+    module_name='dramatiq.brokers.redis',
+    class_name='RedisBroker',
+    default_url='redis://127.0.0.1:6379/0',
+)
