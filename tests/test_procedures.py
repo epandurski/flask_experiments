871d440e22b86c67cb14a468d38af2c0dc83e047
Evgeni Pandurski <epandurski@gmail.com>
2019-04-08 16:00:25 +0300

    Remove unused procedure

--- tests/test_procedures.py
+++ tests/test_procedures.py
@@ -13,29 +13,6 @@ def test_create_debtor(db_session):
     assert len(debtor.account_list) == 1
 
 
-def test_prepare_withdrawal(db_session):
-    debtor = procedures.create_debtor(user_id=666)
-    debtor = Debtor.query.filter_by(debtor_id=debtor.debtor_id).one()
-    len(debtor.withdrawal_request_list) == 0
-    operator = debtor.operator_list[0]
-    deadline_ts = datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(days=30)
-    request = procedures.create_withdrawal_request(operator, 777, 1000, deadline_ts)
-    debtor = Debtor.query.filter_by(debtor_id=debtor.debtor_id).one()
-    len(debtor.withdrawal_request_list) == 1
-    with pytest.raises(procedures.InsufficientFunds):
-        procedures.prepare_withdrawal(request)
-    db_session.add(Account(debtor=debtor, creditor_id=777, balance=3000, avl_balance=3000))
-    payment = procedures.prepare_withdrawal(request)
-    payment = procedures.prepare_withdrawal(request)
-    assert payment.amount == 1000
-    procedures.commit_creditor_prepared_transfer(payment)
-    assert len(WithdrawalRequest.query.all()) == 0
-    assert len(Withdrawal.query.all()) == 1
-    a = Account.query.filter_by(debtor_id=debtor.debtor_id, creditor_id=777).one()
-    assert a.balance == 2000
-    assert a.avl_balance == 2000
-
-
 def test_prepare_direct_transfer(db_session):
     @db.execute_atomic
     def transfer():
